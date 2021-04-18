fa8ea556ab15735d07129d6933b08d409c4dbdb0
vDocker <k304189@gmail.com>
2021-04-18 17:30:04 +0900

    First commit

---
+++ setDockerEnv.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+touch .env
+echo "UID=$(id -u $USER)" >> .env
+echo "GID=$(id -g $USER)" >> .env
+echo "UNAME=$USER" >> .env
+echo "MYSQL_ROOT_PASSWORD=password" >> .env
+echo "DB_OPEN_PORT=4306" >> .env
+echo "MYSQL_PORT=3306" >> .env
+echo "CONTAINER_PORT=3000" >> .env
+echo "API_PORT=3000" >> .env
+echo "FRONT_PORT=8000" >> .env
