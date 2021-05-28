732eccd23f69ab56f08c9f893264cf4229a7e3f2
vDocker <k304189@gmail.com>
2021-05-28 18:39:27 +0900

    Usageのデータ取得中にロード用のスピナーを表示するように修正

--- front/src/components/organisms/stock/UsageListTable.tsx
+++ front/src/components/organisms/stock/UsageListTable.tsx
@@ -1,5 +1,5 @@
 import { memo, useEffect, useState, VFC } from "react";
-import { Box, Flex, Table, Tbody, Thead, Tr, Td } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Table, Tbody, Thead, Tr, Td } from "@chakra-ui/react";
 
 import { DefaultPaging } from "../../atoms/button/DefaultPaging";
 import { Stock } from "../../../types/api/stock";
@@ -22,6 +22,7 @@ export const UsageListTable: VFC<Props> = memo((props) => {
   const { stock = null } = props;
   const [historyPagingOffset, setHistoryPagingOffset] = useState(0);
   const [foodstuffPagingOffset, setFoodstuffPagingOffset] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const historyPagingDisplayNum = 5;
   const onChangeHistoryPage = (page: {selected: number}) =>
@@ -36,101 +37,126 @@ export const UsageListTable: VFC<Props> = memo((props) => {
       getHistoryUsage(stock.id)
         .catch(() => {
           showMessage({ title: "使用履歴の取得が失敗しました", status: "error" });
+        })
+        .finally(() => {
+          if (stock.stock_type === "料理") {
+            getFoodstuffUsage(stock.id)
+              .catch(() => {
+                showMessage({ title: "使用食材の取得が失敗しました", status: "error" });
+              })
+              .finally(() => {
+                setLoading(false);
+              });
+          } else {
+            setLoading(false);
+          }
         });
-      if (stock.stock_type === "料理") {
-        getFoodstuffUsage(stock.id)
-          .catch(() => {
-            showMessage({ title: "使用食材の取得が失敗しました", status: "error" });
-          });
-        console.log(foodstuffUsages);
-      }
+    } else {
+      setLoading(false);
     }
   }, []);
 
   return (
-    <Box>
-      <Box>
-        <Box className="sectionTitle">
-          使用履歴
-        </Box>
-        {historyUsages.length > 0 ? (
-          <>
-            <Flex justify="end">
-              <DefaultPaging
-                displayNum={historyPagingDisplayNum}
-                dataNum={historyUsages.length}
-                onPageChange={onChangeHistoryPage}
-              />
-            </Flex>
-            <Table size="sm">
-              <Thead>
-                <Tr>
-                  <Td w="10%">使用日</Td>
-                  <Td w="20%">使用タイプ</Td>
-                  <Td w="15%">使用率</Td>
-                  <Td w="55%">一言メモ</Td>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {historyUsages
-                  .slice(historyPagingOffset, historyPagingOffset + historyPagingDisplayNum)
-                  .map((data) => (
-                    <Tr key={data.id}>
-                      <Td w="10%">{data.use_date}</Td>
-                      <Td w="20%">{data.use_type}</Td>
-                      <Td w="15%">{data.use_rate}%</Td>
-                      <Td w="55%">{data.note}</Td>
-                    </Tr>
-                  ))}
-              </Tbody>
-            </Table>
-          </>
-        ) : (
-          <Box>使用履歴はありません</Box>
-        )}
-      </Box>
-      <Box mt={10}>
-        {foodstuffUsages.length > 0 ? (
-          <>
+    <Box h="100%" ml={2}>
+      {loading ? (
+        <Flex h="100%" justify="center" align="center">
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        </Flex>
+      ) : (
+        <Box>
+          <Box>
             <Box className="sectionTitle">
-              材料
+              使用履歴
             </Box>
-            <Flex justify="end">
-              <DefaultPaging
-                displayNum={foodstuffPagingDisplayNum}
-                dataNum={foodstuffUsages.length}
-                onPageChange={onChangeFoodstuffPage}
-              />
-            </Flex>
-            <Table size="sm">
-              <Thead>
-                <Tr>
-                  <Td w="40%">食材名</Td>
-                  <Td w="20%">カテゴリー</Td>
-                  <Td w="15%">価格</Td>
-                  <Td w="20%">カロリー</Td>
-                  <Td w="5%">量</Td>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {foodstuffUsages
-                  .slice(foodstuffPagingOffset, foodstuffPagingOffset + foodstuffPagingDisplayNum)
-                  .map((data) => (
-                    <Tr key={data.id}>
-                      <Td w="40%">{data.name}</Td>
-                      <Td w="20%">{data.category}</Td>
-                      <Td w="15%">{data.price}円</Td>
-                      <Td w="20%">{data.kcal}kcal</Td>
-                      <Td w="5%">{data.amount}{data.unit}</Td>
+            {historyUsages.length > 0 ? (
+              <>
+                <Flex justify="end">
+                  <DefaultPaging
+                    displayNum={historyPagingDisplayNum}
+                    dataNum={historyUsages.length}
+                    onPageChange={onChangeHistoryPage}
+                  />
+                </Flex>
+                <Table size="sm">
+                  <Thead>
+                    <Tr>
+                      <Td w="10%">使用日</Td>
+                      <Td w="20%">使用タイプ</Td>
+                      <Td w="15%">使用率</Td>
+                      <Td w="55%">一言メモ</Td>
                     </Tr>
-                  ))}
-              </Tbody>
-            </Table>
-          </>
-        ) : (
-          <></>
-        )}
-      </Box>
+                  </Thead>
+                  <Tbody>
+                    {historyUsages
+                      .slice(historyPagingOffset, historyPagingOffset + historyPagingDisplayNum)
+                      .map((data) => (
+                        <Tr key={data.id}>
+                          <Td w="10%">{data.use_date}</Td>
+                          <Td w="20%">{data.use_type}</Td>
+                          <Td w="15%">{data.use_rate}%</Td>
+                          <Td w="55%">{data.note}</Td>
+                        </Tr>
+                      ))}
+                  </Tbody>
+                </Table>
+              </>
+            ) : (
+              <Box>使用履歴はありません</Box>
+            )}
+          </Box>
+          <Box mt={10}>
+            {foodstuffUsages.length > 0 ? (
+              <>
+                <Box className="sectionTitle">
+                  材料
+                </Box>
+                <Flex justify="end">
+                  <DefaultPaging
+                    displayNum={foodstuffPagingDisplayNum}
+                    dataNum={foodstuffUsages.length}
+                    onPageChange={onChangeFoodstuffPage}
+                  />
+                </Flex>
+                <Table size="sm">
+                  <Thead>
+                    <Tr>
+                      <Td w="40%">食材名</Td>
+                      <Td w="20%">カテゴリー</Td>
+                      <Td w="15%">価格</Td>
+                      <Td w="20%">カロリー</Td>
+                      <Td w="5%">量</Td>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {foodstuffUsages
+                      .slice(
+                        foodstuffPagingOffset,
+                        foodstuffPagingOffset + foodstuffPagingDisplayNum,
+                      )
+                      .map((data) => (
+                        <Tr key={data.id}>
+                          <Td w="40%">{data.name}</Td>
+                          <Td w="20%">{data.category}</Td>
+                          <Td w="15%">{data.price}円</Td>
+                          <Td w="20%">{data.kcal}kcal</Td>
+                          <Td w="5%">{data.amount}{data.unit}</Td>
+                        </Tr>
+                      ))}
+                  </Tbody>
+                </Table>
+              </>
+            ) : (
+              <></>
+            )}
+          </Box>
+        </Box>
+      )}
     </Box>
   );
 });
