9021a82ae20bcd8b00d3c672fd798647c87fb590
vDocker <k304189@gmail.com>
2021-06-02 20:38:51 +0900

    カレンダーの表示月を追加　別の月のカレンダーを表示できるようコンポーネントを追加

--- front/src/components/pages/calendar/Calendar.tsx
+++ front/src/components/pages/calendar/Calendar.tsx
@@ -1,28 +1,64 @@
-import { memo, VFC, useEffect, useState } from "react";
-import { Box, Table, Thead, Tbody, Td, Tr } from "@chakra-ui/react";
+import { ChangeEvent, memo, VFC, useEffect, useState } from "react";
+import { Box, Center, Grid, GridItem, Table, Thead, Tbody, Td, Tr } from "@chakra-ui/react";
 
 import { DefaultLink } from "../../atoms/button/DefaultLink";
+import { SecondaryButton } from "../../atoms/button/SecondaryButton";
+import { DefaultInput } from "../../atoms/input/DefaultInput";
 
 import { useCalendar } from "../../../hooks/common/useCalendar";
 import { CalendarWeekType } from "../../../types/pages/calendar/calendarWeekType";
 import { SigninHeaderLayout } from "../../templates/SigninHeaderLayout";
 
 export const Calendar: VFC = memo(() => {
-  const [monthlyCalendar, setMonthlyCalendar] = useState<Array<CalendarWeekType>>();
   const {
     weekDayArray,
     getFormatYearMonth,
     getCalendarArray,
   } = useCalendar();
+  const [monthlyCalendar, setMonthlyCalendar] = useState<Array<CalendarWeekType>>();
+  const [displayYearMonth, setDisplayYearMonth] = useState(getFormatYearMonth(new Date()));
+
+  const onChangeDisplayYearMonth = (e: ChangeEvent<HTMLInputElement>) => {
+    setDisplayYearMonth(e.target.value);
+  };
+
+  const addDisplayYearMonth = (addMonth:number) => {
+    const [year, month] = displayYearMonth.split("-").map(Number);
+    setDisplayYearMonth(getFormatYearMonth(new Date(year, month + addMonth, 1)));
+  };
 
   useEffect(() => {
-    setMonthlyCalendar(getCalendarArray(getFormatYearMonth(new Date())));
-  }, []);
+    setMonthlyCalendar(getCalendarArray(displayYearMonth));
+  }, [displayYearMonth]);
 
   return (
     <SigninHeaderLayout title="カレンダー">
       <Box as="article" w="100%" h="100%">
-        <Box w="100%" h="10%">ボタンエリア</Box>
+        <Box h="10%">
+          <Grid
+            templateColumns="repeat(14, 1fr)"
+            gap={1}
+          >
+            <GridItem colSpan={1}>
+              <Center h="100%">表示月</Center>
+            </GridItem>
+            <GridItem colSpan={1}>
+              <DefaultInput
+                type="month"
+                value={displayYearMonth}
+                onChange={onChangeDisplayYearMonth}
+              />
+            </GridItem>
+            <GridItem colSpan={2}>
+              <SecondaryButton onClick={() => { addDisplayYearMonth(-2); }}>
+                先月
+              </SecondaryButton>
+              <SecondaryButton onClick={() => { addDisplayYearMonth(0); }}>
+                翌月
+              </SecondaryButton>
+            </GridItem>
+          </Grid>
+        </Box>
         {monthlyCalendar ? (
           <Table className="calendar" w="100%" h="90%">
             <Thead w="100%" h="4%">
